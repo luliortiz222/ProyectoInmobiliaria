0ecdec91fdf14e80be09ea3eccedb4aff4efe1f8
fabricio silva <fabridanisilva@gmail.com>
2026-08-13 16:39:07 -0300

    variables en la clase propietario

--- ProyectoInmobiliaria/ProyectoInmobiliaria/Propietario.cs
+++ ProyectoInmobiliaria/ProyectoInmobiliaria/Propietario.cs
@@ -2,5 +2,10 @@
 
 public class Propietario
 {
-	
+    public int Id { get; set; }
+    public string Dni { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public string Telefono { get; set; }
+    public string Email { get; set; }
 }
