c2a84476ce94e56f5d902bb5396e993facac1656
fabricio silva <fabridanisilva@gmail.com>
2026-08-13 16:39:07 -0300

    agregue el constructor en propietarioagregue el constructor en Propietario

--- ProyectoInmobiliaria/ProyectoInmobiliaria/Propietario.cs
+++ ProyectoInmobiliaria/ProyectoInmobiliaria/Propietario.cs
@@ -2,5 +2,22 @@
 
 public class Propietario
 {
-	
+    public int Id { get; set; }
+    public string Dni { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public string Telefono { get; set; }
+    public string Email { get; set; }
+
+
+
+    public Propietario(int id, string dni, string nombre, string apellido, string telefono, string email)
+    {
+        Id = id;
+        Dni = dni;
+        Nombre = nombre;
+        Apellido = apellido;
+        Telefono = telefono;
+        Email = email;
+    }
 }
