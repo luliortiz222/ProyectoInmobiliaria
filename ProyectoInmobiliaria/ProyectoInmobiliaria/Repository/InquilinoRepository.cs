6b9baba19961615f94fa48bc831195420512276b
Lourdes <luliortiz222@gmail.com>
2026-08-19 15:03:01 -0300

    en InquilinoRepository se agregaron los metodos restantes de ls operaciones AMB

--- ProyectoInmobiliaria/ProyectoInmobiliaria/Repository/InquilinoRepository.cs
+++ ProyectoInmobiliaria/ProyectoInmobiliaria/Repository/InquilinoRepository.cs
@@ -77,4 +77,96 @@ public class InquilinoRepository
         }
         return lista;
     }
+
+    public Inquilino ObtenerPorId(int id)
+    {
+        Inquilino i = null;
+        string query = "SELECT * FROM Inquilino WHERE IdInquilino = @Id";
+
+        using (MySqlConnection conexion = new MySqlConnection(_cadenaConexion))
+        {
+            using (MySqlCommand comando = new MySqlCommand(query, conexion))
+            {
+                comando.Parameters.AddWithValue("@Id", id);
+
+                try
+                {
+                    conexion.Open();
+                    using (MySqlDataReader reader = comando.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            i = new Inquilino
+                            {
+                                IdInquilino = Convert.ToInt32(reader["IdInquilino"]),
+                                Dni = reader["Dni"].ToString(),
+                                Nombre = reader["Nombre"].ToString(),
+                                Apellido = reader["Apellido"].ToString(),
+                                Email = reader["Email"].ToString(),
+                                Telefono = reader["Telefono"].ToString()
+                            };
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al obtener por ID: " + ex.Message);
+                }
+            }
+        }
+        return i;
+    }
+
+    public void Modificar(Inquilino inquilino)
+    {
+        string query = @"UPDATE Inquilino 
+                        SET Dni = @Dni, Nombre = @Nombre, Apellido = @Apellido, Email = @Email, Telefono = @Telefono 
+                        WHERE IdInquilino = @Id";
+
+        using (MySqlConnection conexion = new MySqlConnection(_cadenaConexion))
+        {
+            using (MySqlCommand comando = new MySqlCommand(query, conexion))
+            {
+                comando.Parameters.AddWithValue("@Id", inquilino.IdInquilino);
+                comando.Parameters.AddWithValue("@Dni", inquilino.Dni);
+                comando.Parameters.AddWithValue("@Nombre", inquilino.Nombre);
+                comando.Parameters.AddWithValue("@Apellido", inquilino.Apellido);
+                comando.Parameters.AddWithValue("@Email", inquilino.Email);
+                comando.Parameters.AddWithValue("@Telefono", inquilino.Telefono);
+
+                try
+                {
+                    conexion.Open();
+                    comando.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al modificar inquilino: " + ex.Message);
+                }
+            }
+        }
+    }
+
+    public void Eliminar(int id)
+    {
+        string query = "DELETE FROM Inquilino WHERE IdInquilino = @Id";
+
+        using (MySqlConnection conexion = new MySqlConnection(_cadenaConexion))
+        {
+            using (MySqlCommand comando = new MySqlCommand(query, conexion))
+            {
+                comando.Parameters.AddWithValue("@Id", id);
+
+                try
+                {
+                    conexion.Open();
+                    comando.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al eliminar inquilino: " + ex.Message);
+                }
+            }
+        }
+    }
 }
