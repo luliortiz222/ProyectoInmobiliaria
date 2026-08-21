c03ea6144ff62c8837540b7245e5ff3c7858b437
Lourdes <luliortiz222@gmail.com>
2026-08-20 21:16:21 -0300

    Se modificaron inquilino y propieario en models y se lleno index.cshtml

--- ProyectoInmobiliaria/ProyectoInmobiliaria/models/Inquilino.cs
+++ ProyectoInmobiliaria/ProyectoInmobiliaria/models/Inquilino.cs
@@ -1,25 +1,28 @@
 ﻿using System;
 
-public class Inquilino
+namespace ProyectoInmobiliaria.models
 {
-    public int IdInquilino{ get; set; }
-    public string Dni { get; set; }
-    public string Nombre { get; set; }
-    public string Apellido { get; set; }
-    public string Email { get; set; }
-    public string Telefono { get; set; }
-
-    public Inquilino (int idInquilino, string dni, string nombre, string apellido, string telefono, string email)
+    public class Inquilino
     {
-        IdInquilino = idInquilino;
-        Dni = dni;
-        Nombre = nombre;
-        Apellido = apellido;
-        Telefono = telefono;
-        Email = email;
-    }
-    public Inquilino()
-    {
-    }
+        public int IdInquilino { get; set; }
+        public string Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
 
+        public Inquilino(int idInquilino, string dni, string nombre, string apellido, string telefono, string email)
+        {
+            IdInquilino = idInquilino;
+            Dni = dni;
+            Nombre = nombre;
+            Apellido = apellido;
+            Telefono = telefono;
+            Email = email;
+        }
+
+        public Inquilino()
+        {
+        }
+    }
 }
