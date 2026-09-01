9bf5d6aa02a2952d241d715efa3ffed6494eab03
fabricio silva <fabridanisilva@gmail.com>
2026-09-01 15:29:53 -0300

    Entidades Inmueble y tipoInmueble

---
+++ ProyectoInmobiliaria/ProyectoInmobiliaria/models/Inmueble.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ProyectoInmoviliaria.Models
+{
+	public class Inmueble	
+	{
+		public int idInmueble { get; set; }
+		public string direccion { get; set; }
+		public int cantAmbientes { get; set; }
+		public double superficie { get; set; }
+		public double precipPorDia { get; set; }
+		public char imagenPortada { get; set; }
+		public int idPropietario { get; set; }
+		public int idInmueble { get; set; }
+
+        public Inmueble(int idInmueble, string direccion, int cantAmbientes, double superficie, double precipPorDia, char imagenPortada, int idPropietario)
+		{
+			this.idInmueble = idInmueble;
+			this.direccion = direccion;
+			this.cantAmbientes = cantAmbientes;
+			this.superficie = superficie;
+			this.precipPorDia = precipPorDia;
+			this.imagenPortada = imagenPortada;
+			this.idPropietario = idPropietario;
+		}
+        public Inmueble()
+        {
+        }
+
+		public override string ToString()
+        {
+            return $"Inmueble [idInmueble={idInmueble}, direccion={direccion}, cantAmbientes={cantAmbientes}, superficie={superficie}, precipPorDia={precipPorDia}, imagenPortada={imagenPortada}, idPropietario={idPropietario}]";
+        }
+    }
---
+++ ProyectoInmobiliaria/ProyectoInmobiliaria/models/TipoInmueble.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ProyectoInmoviliaria.Models
+{
+	public class TipoInmueble
+	{
+		public int IdTipo { get; set; }
+		public string nombre { get; set; }
+
+		public TipoInmueble(int idTipo, string nombre)
+		{
+			IdTipo = idTipo;
+			nombre = nombre;
+		}
+		public TipoInmueble() { 
+		
+		}
+    }
+}
