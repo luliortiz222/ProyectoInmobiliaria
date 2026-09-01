c2be7777b8034b9070608b60258883a37a155d08
Lourdes <luliortiz222@gmail.com>
2026-09-01 15:51:15 -0300

    Entidad Reserva

--- ProyectoInmobiliaria/ProyectoInmobiliaria/models/Inmueble.cs
+++ ProyectoInmobiliaria/ProyectoInmobiliaria/models/Inmueble.cs
@@ -4,31 +4,31 @@ namespace ProyectoInmoviliaria.Models
 {
 	public class Inmueble	
 	{
-		public int idInmueble { get; set; }
+		public int IdInmueble { get; set; }
 		public string direccion { get; set; }
 		public int cantAmbientes { get; set; }
 		public double superficie { get; set; }
 		public double precipPorDia { get; set; }
 		public char imagenPortada { get; set; }
 		public int idPropietario { get; set; }
-		public int idInmueble { get; set; }
+		public int IdTipo { get; set; }
 
-        public Inmueble(int idInmueble, string direccion, int cantAmbientes, double superficie, double precipPorDia, char imagenPortada, int idPropietario)
+        public Inmueble(int IdInmueble, string direccion, int cantAmbientes, double superficie, double precipPorDia, char imagenPortada, int idPropietario, int idTipo)
 		{
-			this.idInmueble = idInmueble;
+			this.IdInmueble = IdInmueble;
 			this.direccion = direccion;
 			this.cantAmbientes = cantAmbientes;
 			this.superficie = superficie;
 			this.precipPorDia = precipPorDia;
 			this.imagenPortada = imagenPortada;
 			this.idPropietario = idPropietario;
-		}
-        public Inmueble()
-        {
+			this.IdTipo= idTipo; 
+        }
+        public Inmueble() {
         }
 
 		public override string ToString()
         {
-            return $"Inmueble [idInmueble={idInmueble}, direccion={direccion}, cantAmbientes={cantAmbientes}, superficie={superficie}, precipPorDia={precipPorDia}, imagenPortada={imagenPortada}, idPropietario={idPropietario}]";
+            return $"Inmueble [IdInmueble={IdInmueble}, direccion={direccion}, cantAmbientes={cantAmbientes}, superficie={superficie}, precipPorDia={precipPorDia}, imagenPortada={imagenPortada}, idPropietario={idPropietario}], IdTipo={IdTipo}]";
         }
     }
---
+++ ProyectoInmobiliaria/ProyectoInmobiliaria/models/Reserva.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ProyectoInmobiliaria.models
+{
+    public class Reserva
+    {
+        public int IdReserva { get; set; }
+        public int IdInquilino { get; set; }
+        public int IdInmueble { get; set; }
+        public decimal MontoPorDia { get; set; }
+        public DateTime FechaDesde { get; set; }
+        public DateTime FechaHasta { get; set; }
+    }
+}
