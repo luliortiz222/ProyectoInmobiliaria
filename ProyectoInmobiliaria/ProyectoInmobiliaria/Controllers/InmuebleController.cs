bbc65a221b089377ab8f917e07e875ce9c07769d
Lourdes <luliortiz222@gmail.com>
2026-09-01 18:28:20 -0300

    Se creó TipoInmueblesController y se corrigió InmuebleController. Se ajustaron los métodos según el repositorio de Propietario y se agregó la acción Details.

---
+++ ProyectoInmobiliaria/ProyectoInmobiliaria/Controllers/InmuebleController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoInmobiliaria.models;
+using ProyectoInmobiliaria.Repository;
+using System;
+
+namespace ProyectoInmobiliaria.Controllers
+{
+    public class InmuebleController : Controller
+    {
+        private readonly InmuebleRepository _inmuebleRepository;
+        private readonly PropietarioRepository _propietarioRepo;
+        private readonly TipoInmuebleRepository _tipoRepo;
+
+        public InmuebleController(
+            InmuebleRepository inmuebleRepository,
+            PropietarioRepository propietarioRepo,
+            TipoInmuebleRepository tipoRepo)
+        {
+            _inmuebleRepository = inmuebleRepository;
+            _propietarioRepo = propietarioRepo;
+            _tipoRepo = tipoRepo;
+        }
+
+        // GET: /Inmueble
+        public IActionResult Index()
+        {
+            var inmuebles = _inmuebleRepository.ObtenerTodos();
+            return View(inmuebles);
+        }
+
+        // GET: /Inmueble/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Propietarios = _propietarioRepo.obtenerTodos();
+            ViewBag.TiposInmueble = _tipoRepo.ObtenerTodos();
+
+            return View();
+        }
+
+        // POST: /Inmueble/Create
+        [HttpPost]
+        public IActionResult Create(Inmueble inmueble)
+        {
+            _inmuebleRepository.Guardar(inmueble);
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: /Inmueble/Edit/5
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Inmueble inmueble = _inmuebleRepository.ObtenerPorId(id);
+
+            if (inmueble == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Propietarios = _propietarioRepo.obtenerTodos();
+            ViewBag.TiposInmueble = _tipoRepo.ObtenerTodos();
+
+            return View(inmueble);
+        }
+
+        // POST: /Inmueble/Edit
+        [HttpPost]
+        public IActionResult Edit(Inmueble inmueble)
+        {
+            _inmuebleRepository.Actualizar(inmueble);
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: /Inmueble/Details/5
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var inmueble = _inmuebleRepository.ObtenerPorId(id);
+
+            if (inmueble == null)
+            {
+                return NotFound();
+            }
+
+            return View(inmueble);
+        }
+
+        // GET: /Inmueble/Borrar/5
+        [HttpGet]
+        public IActionResult Borrar(int id)
+        {
+            var inmueble = _inmuebleRepository.ObtenerPorId(id);
+
+            if (inmueble == null)
+            {
+                return NotFound();
+            }
+
+            return View(inmueble);
+        }
+
+        // POST: /Inmueble/BorrarConfirmado
+        [HttpPost]
+        public IActionResult BorrarConfirmado(int IdInmueble)
+        {
+            _inmuebleRepository.Eliminar(IdInmueble);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ProyectoInmobiliaria/ProyectoInmobiliaria/Controllers/TipoInmuebleController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoInmobiliaria.models;
+using ProyectoInmobiliaria.Repository;
+
+namespace ProyectoInmobiliaria.Controllers
+{
+    public class TipoInmueblesController : Controller
+    {
+        private readonly TipoInmuebleRepository _tipoInmuebleRepository;
+
+        public TipoInmueblesController(TipoInmuebleRepository tipoInmuebleRepository)
+        {
+            _tipoInmuebleRepository = tipoInmuebleRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var lista = _tipoInmuebleRepository.ObtenerTodos();
+
+            return View(lista);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TipoInmueble tipo)
+        {
+            _tipoInmuebleRepository.Guardar(tipo);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var tipo = _tipoInmuebleRepository.ObtenerPorId(id);
+
+            if (tipo == null)
+            {
+                return NotFound();
+            }
+
+            return View(tipo);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TipoInmueble tipo)
+        {
+            _tipoInmuebleRepository.Editar(tipo);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var tipo = _tipoInmuebleRepository.ObtenerPorId(id);
+
+            if (tipo == null)
+            {
+                return NotFound();
+            }
+
+            return View(tipo);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var tipo = _tipoInmuebleRepository.ObtenerPorId(id);
+
+            if (tipo == null)
+            {
+                return NotFound();
+            }
+
+            return View(tipo);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _tipoInmuebleRepository.Eliminar(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
