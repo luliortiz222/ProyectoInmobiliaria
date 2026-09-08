c4aaa85d716b689fbe55fb0d1b3407fb217fb339
fabricio silva <fabridanisilva@gmail.com>
2026-09-08 19:40:27 -0300

    Pagocontroller funcionando

---
+++ ProyectoInmobiliaria/ProyectoInmobiliaria/Controllers/PagoController.cs
@@ -0,0 +1,53 @@
+﻿using ProyectoInmobiliaria.models;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoInmobiliaria.Repository;
+using System;
+
+namespace ProyectoInmobiliaria.Controllers
+{
+    public class PagoController : Controller
+    {
+        private readonly PagoRepository _pagoRepository;
+        public PagoController(PagoRepository pagoRepository)
+        {
+            _pagoRepository = pagoRepository;
+        }
+
+
+        [HttpGet]
+        public IActionResult PorReserva(int id)
+        {
+            ViewBag.idReserva = id;
+            var lista = _pagoRepository.ObtenerPorReserva(id);
+            return View(lista);
+        }
+
+
+        //crear pago
+        [HttpGet]
+        public IActionResult Crear(int id)
+        {
+            var pago = new Pago()
+            { idReserva = id,
+            fechaPago = DateTime.Now.Date };
+            return View(pago);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Pago pago)
+        {
+            try
+            {
+                pago.idUsuarioCreador = 1;
+                _pagoRepository.GuardarPago(pago);
+
+                return RedirectToAction("PorReserva", new { id = pago.idReserva });
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Error al hacer el pago: " + ex.ToString());
+                return View(pago);
+            }
+        }
+
+    }
+}
