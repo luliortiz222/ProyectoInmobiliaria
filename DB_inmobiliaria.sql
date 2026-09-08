0bf54e648e11557b9424c5f72dee07e9573ca70b
Lourdes <luliortiz222@gmail.com>
2026-09-08 15:48:06 -0300

    Se agregó la tabla Usuario y tabla Pago

--- DB_inmobiliaria.sql
+++ DB_inmobiliaria.sql
@@ -24,6 +24,16 @@ CREATE TABLE TipoInmueble (
     Nombre VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE Usuario (
+    IdUsuario INT AUTO_INCREMENT PRIMARY KEY,
+    Email VARCHAR(150) UNIQUE NOT NULL,
+    Password VARCHAR(255) NOT NULL,
+    Nombre VARCHAR(100) NOT NULL,
+    Apellido VARCHAR(100) NOT NULL,
+    Avatar VARCHAR(300),
+    Rol VARCHAR(20) NOT NULL
+);
+
 CREATE TABLE Inmueble (
     IdInmueble INT AUTO_INCREMENT PRIMARY KEY,
     Direccion VARCHAR(200) NOT NULL,
@@ -34,6 +44,7 @@ CREATE TABLE Inmueble (
     Estado BOOLEAN NOT NULL DEFAULT TRUE, 
     IdPropietario INT NOT NULL,
     IdTipoInmueble INT NOT NULL,
+    PorcentajeReserva DECIMAL(5,2) NOT NULL DEFAULT 0,
 
     FOREIGN KEY (IdPropietario) REFERENCES Propietario(IdPropietario),
     FOREIGN KEY (IdTipoInmueble) REFERENCES TipoInmueble(IdTipoInmueble)
@@ -46,8 +57,29 @@ CREATE TABLE Reserva (
     MontoPorDia DECIMAL(10,2) NOT NULL,
     FechaDesde DATE NOT NULL,
     FechaHasta DATE NOT NULL,
+    IdUsuarioCreador INT NOT NULL,
+    IdUsuarioFinalizador INT NULL,
+    FechaFinalizacion DATE NULL,
 
     FOREIGN KEY (IdInquilino) REFERENCES Inquilino(IdInquilino),
-    FOREIGN KEY (IdInmueble) REFERENCES Inmueble(IdInmueble)
+    FOREIGN KEY (IdInmueble) REFERENCES Inmueble(IdInmueble),
+    FOREIGN KEY (IdUsuarioCreador) REFERENCES Usuario(IdUsuario),
+    FOREIGN KEY (IdUsuarioFinalizador) REFERENCES Usuario(IdUsuario)
+
+);
+
+CREATE TABLE Pago (
+    IdPago INT AUTO_INCREMENT PRIMARY KEY,
+    IdReserva INT NOT NULL,
+    Concepto VARCHAR(200) NOT NULL,
+    FechaPago DATE NOT NULL,
+    Importe DECIMAL(10,2) NOT NULL,
+    Estado BOOLEAN NOT NULL DEFAULT TRUE,
+    IdUsuarioCreador INT NOT NULL,
+    IdUsuarioAnulador INT NULL,
+
+    FOREIGN KEY (IdReserva) REFERENCES Reserva(IdReserva),
+    FOREIGN KEY (IdUsuarioCreador) REFERENCES Usuario(IdUsuario),
+    FOREIGN KEY (IdUsuarioAnulador) REFERENCES Usuario(IdUsuario)
 );
 
